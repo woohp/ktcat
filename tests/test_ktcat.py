15ce1d3e98987499debc600591cc4a2a138fd230
Hui Peng Hu <woohp135@gmail.com>
2026-04-26 19:30:12 -0700

    polish

--- tests/test_ktcat.py
+++ tests/test_ktcat.py
@@ -5,31 +5,31 @@ from PIL import Image
 from ktcat import ktcat
 
 
-def test_ktcat_pil():
+def test_ktcat_pil() -> None:
     img = Image.new("RGB", (10, 10), color="red")
     ktcat(img)
 
 
-def test_ktcat_numpy_hwc():
+def test_ktcat_numpy_hwc() -> None:
     img = np.zeros((10, 10, 3), dtype=np.uint8)
     ktcat(img)
 
 
-def test_ktcat_numpy_chw():
+def test_ktcat_numpy_chw() -> None:
     img = np.zeros((3, 10, 10), dtype=np.uint8)
     ktcat(img)
 
 
-def test_ktcat_torch_chw():
+def test_ktcat_torch_chw() -> None:
     img = torch.zeros((3, 10, 10))
     ktcat(img)
 
 
-def test_ktcat_torch_hcw():
+def test_ktcat_torch_hcw() -> None:
     img = torch.zeros((10, 10, 3))
     ktcat(img)
 
 
-def test_ktcat_float():
+def test_ktcat_float() -> None:
     img = np.random.rand(10, 10, 3).astype(np.float32)
     ktcat(img)
